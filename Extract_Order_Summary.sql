28d31ab288456110e7baa1edf8cb351935b9c3b9
Alexandre Dubois <contact@alexandredubois.com>
2022-05-23 12:10:56 +0200

    Clean comment, add variables

--- Extract_Order_Summary.sql
+++ Extract_Order_Summary.sql
@@ -1,34 +1,24 @@
+SET @begin_date = '2018-01-01';
+SET @end_date = '2023-08-01';
+
 select
     p.ID as order_id,
     p.post_date,
-    /*max( CASE WHEN pm.meta_key = '_billing_email' and p.ID = pm.post_id THEN pm.meta_value END ) as billing_email,
-    max( CASE WHEN pm.meta_key = '_billing_first_name' and p.ID = pm.post_id THEN pm.meta_value END ) as _billing_first_name,
-    max( CASE WHEN pm.meta_key = '_billing_last_name' and p.ID = pm.post_id THEN pm.meta_value END ) as _billing_last_name,
-    max( CASE WHEN pm.meta_key = '_billing_address_1' and p.ID = pm.post_id THEN pm.meta_value END ) as _billing_address_1,
-    max( CASE WHEN pm.meta_key = '_billing_address_2' and p.ID = pm.post_id THEN pm.meta_value END ) as _billing_address_2,
-    max( CASE WHEN pm.meta_key = '_billing_city' and p.ID = pm.post_id THEN pm.meta_value END ) as _billing_city,
-    max( CASE WHEN pm.meta_key = '_billing_state' and p.ID = pm.post_id THEN pm.meta_value END ) as _billing_state,
-    max( CASE WHEN pm.meta_key = '_billing_postcode' and p.ID = pm.post_id THEN pm.meta_value END ) as _billing_postcode,
-    max( CASE WHEN pm.meta_key = '_shipping_first_name' and p.ID = pm.post_id THEN pm.meta_value END ) as _shipping_first_name,
-    max( CASE WHEN pm.meta_key = '_shipping_last_name' and p.ID = pm.post_id THEN pm.meta_value END ) as _shipping_last_name,
-    max( CASE WHEN pm.meta_key = '_shipping_address_1' and p.ID = pm.post_id THEN pm.meta_value END ) as _shipping_address_1,
-    max( CASE WHEN pm.meta_key = '_shipping_address_2' and p.ID = pm.post_id THEN pm.meta_value END ) as _shipping_address_2,*/
-    max( CASE WHEN pm.meta_key = '_shipping_city' and p.ID = pm.post_id THEN pm.meta_value END ) as _shipping_city,
-    max( CASE WHEN pm.meta_key = '_shipping_country' and p.ID = pm.post_id THEN pm.meta_value END ) as _shipping_country,
-    /*max( CASE WHEN pm.meta_key = '_shipping_state' and p.ID = pm.post_id THEN pm.meta_value END ) as _shipping_state,*/
-    max( CASE WHEN pm.meta_key = '_shipping_postcode' and p.ID = pm.post_id THEN pm.meta_value END ) as _shipping_postcode,
-    max( CASE WHEN pm.meta_key = '_order_total' and p.ID = pm.post_id THEN pm.meta_value END ) as order_total,
-    max( CASE WHEN pm.meta_key = '_order_tax' and p.ID = pm.post_id THEN pm.meta_value END ) as order_tax,
-    max( CASE WHEN pm.meta_key = '_order_shipping' and p.ID = pm.post_id THEN pm.meta_value END ) as _order_shipping,
-    max( CASE WHEN pm.meta_key = '_paid_date' and p.ID = pm.post_id THEN pm.meta_value END ) as paid_date,
-    ( select group_concat( order_item_name separator '|' ) from wp_woocommerce_order_items where order_id = p.ID ) as order_items
+    MAX(CASE WHEN pm.meta_key = '_shipping_city' AND p.ID = pm.post_id THEN pm.meta_value END) AS _shipping_city,
+    MAX(CASE WHEN pm.meta_key = '_shipping_country' AND p.ID = pm.post_id THEN pm.meta_value END) AS _shipping_country,
+    MAX(CASE WHEN pm.meta_key = '_shipping_postcode' AND p.ID = pm.post_id THEN pm.meta_value END) AS _shipping_postcode,
+    MAX(CASE WHEN pm.meta_key = '_order_total' AND p.ID = pm.post_id THEN pm.meta_value END) AS order_total,
+    MAX(CASE WHEN pm.meta_key = '_order_tax' AND p.ID = pm.post_id THEN pm.meta_value END) AS order_tax,
+    MAX(CASE WHEN pm.meta_key = '_order_shipping' AND p.ID = pm.post_id THEN pm.meta_value END) AS _order_shipping,
+    MAX(CASE WHEN pm.meta_key = '_paid_date' AND p.ID = pm.post_id THEN pm.meta_value END) AS paid_date,
+    ( select group_concat( order_item_name separator '|' ) FROM wp_woocommerce_order_items WHERE order_id = p.ID) AS order_items
 FROM
     wp_posts p 
-    join wp_postmeta pm on p.ID = pm.post_id
-    join wp_woocommerce_order_items oi on p.ID = oi.order_id
+    JOIN wp_postmeta pm on p.ID = pm.post_id
+    JOIN wp_woocommerce_order_items oi on p.ID = oi.order_id
 WHERE
-    post_type = 'shop_order' and
-    post_date BETWEEN '2018-01-01' AND '2023-08-01' and
-    post_status = 'wc-completed'
+    p.post_type = 'shop_order'
+    AND p.post_date BETWEEN @begin_date AND @end_date 
+	 AND p.post_status = 'wc-completed'
 GROUP BY
     p.ID
