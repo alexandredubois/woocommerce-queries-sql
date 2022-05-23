55bc856b56f5b0d5a99d45c04751c5bf0de2cec6
Alexandre Dubois <contact@alexandredubois.com>
2022-05-23 12:25:26 +0200

    Remove product details for performance concern

--- Extract_Order_Summary.sql
+++ Extract_Order_Summary.sql
@@ -2,17 +2,17 @@ SET @begin_date = '2018-01-01';
 SET @end_date = '2023-08-01';
 
 select
-    p.ID as order_id,
-    p.post_date,
+    p.ID AS order_id,
+    p.post_date AS order_date,
     UPPER(MAX(CASE WHEN pm.meta_key = '_shipping_city' AND p.ID = pm.post_id THEN pm.meta_value END)) AS shipping_city,
     MAX(CASE WHEN pm.meta_key = '_shipping_country' AND p.ID = pm.post_id THEN pm.meta_value END) AS shipping_country,
     MAX(CASE WHEN pm.meta_key = '_shipping_postcode' AND p.ID = pm.post_id THEN pm.meta_value END) AS shipping_postcode,
     MAX(CASE WHEN pm.meta_key = '_order_total' AND p.ID = pm.post_id THEN pm.meta_value END) AS order_total,
     MAX(CASE WHEN pm.meta_key = '_order_tax' AND p.ID = pm.post_id THEN pm.meta_value END) AS order_tax,
-    MAX(CASE WHEN pm.meta_key = '_order_shipping' AND p.ID = pm.post_id THEN pm.meta_value END) AS _order_shipping,
+    MAX(CASE WHEN pm.meta_key = '_order_shipping' AND p.ID = pm.post_id THEN pm.meta_value END) AS order_shipping,
     MAX(CASE WHEN pm.meta_key = '_paid_date' AND p.ID = pm.post_id THEN pm.meta_value END) AS paid_date,
-    MAX(CASE WHEN pm.meta_key = '_payment_method' AND p.ID = pm.post_id THEN pm.meta_value END) AS payment_method,
-    (SELECT group_concat(order_item_name separator '|' ) FROM wp_woocommerce_order_items WHERE order_id = p.ID) AS order_items
+    MAX(CASE WHEN pm.meta_key = '_payment_method' AND p.ID = pm.post_id THEN pm.meta_value END) AS payment_method/*,
+    (SELECT group_concat(order_item_name separator '|' ) FROM wp_woocommerce_order_items WHERE order_id = p.ID) AS order_items*/
 FROM
     wp_posts p 
     JOIN wp_postmeta pm on p.ID = pm.post_id
